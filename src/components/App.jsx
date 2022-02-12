c03f9f7d8a729aeb97edad22a02b18ecb986c4f2
alexlex7 <alekslosev7@gmail.com>
2022-02-12 15:26:56 +0200

    created profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,26 @@
+import Profile from './Profile';
+import user from '../data/user';
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 12,
+    //   textTransform: 'uppercase',
+    //   color: '#010101',
+    // }}
     >
-      React homework template
+      <Profile
+        userName={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
