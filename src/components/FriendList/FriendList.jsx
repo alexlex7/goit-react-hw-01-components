203457ee883cd2560f1a4af8d127b3d1b39051ff
alexlex7 <alekslosev7@gmail.com>
2022-02-23 11:28:14 +0200

    mentors remarks corrected

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -12,7 +12,6 @@ function FriendList({ friends }) {
             avatar={friend.avatar}
             name={friend.name}
             isOnline={friend.isOnline}
-            id={friend.id}
           />
         );
       })}
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,28 +4,26 @@ import getRandomColor from '../../js/randomColor';
 
 export default function Statistics({ title, stats }) {
   return (
-    <>
-      <section className={s.statistics}>
-        {title && <h2 className={s.title}>{title}</h2>}
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
 
-        <ul className={s.statList}>
-          {stats.map(stat => {
-            return (
-              <li
-                className={s.item}
-                key={stat.id}
-                style={{
-                  backgroundColor: getRandomColor(),
-                }}
-              >
-                <span className={s.label}>{stat.label}</span>
-                <span className={s.percentage}>{stat.percentage}%</span>
-              </li>
-            );
-          })}
-        </ul>
-      </section>
-    </>
+      <ul className={s.statList}>
+        {stats.map(stat => {
+          return (
+            <li
+              className={s.item}
+              key={stat.id}
+              style={{
+                backgroundColor: getRandomColor(),
+              }}
+            >
+              <span className={s.label}>{stat.label}</span>
+              <span className={s.percentage}>{stat.percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
   );
 }
 
